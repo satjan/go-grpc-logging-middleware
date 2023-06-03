a49a228448457aedf6dc0d3617586a186f9fe54e
satjan <82696795+satjan@users.noreply.github.com>
2023-06-03 22:49:27 +0800

    Update interceptors.go

--- interceptors.go
+++ interceptors.go
@@ -1,27 +1,26 @@
-package go_grpc_logging_middleware
+package logger
 
 import (
 	"context"
+	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 )
 
 func LoggingInterceptor(logger *logrus.Logger) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		logger.Infof("Received request: %s", info.FullMethod)
-
+		traceID := uuid.New().String()
 		// Log the request body
-		logger.Infof("Request body: %v", req)
+		logger.Infof("%s Method: %s, Request body: %v", traceID, info.FullMethod, req)
 
 		// Invoke the gRPC handler to process the request.
-		resp, err := handler(ctx, req)
+		resp, err := handler(context.WithValue(ctx, "traceID", traceID), req)
 
 		if err != nil {
-			logger.WithError(err).Errorf("Error handling request: %s", err.Error())
+			logger.WithError(err).Errorf("%s Error: %s ", traceID, err.Error())
 		} else {
 			// Log the response body
-			logger.Infof("Response body: %v", resp)
-			logger.Infof("Sent response: %s", info.FullMethod)
+			logger.Infof("%s  Method: %s, Response body: %v", traceID, info.FullMethod, resp)
 		}
 
 		return resp, err
