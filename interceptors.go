d0c11dc7c3c67b4e2c10dc9b368fe8f3b93f275e
satjan <82696795+satjan@users.noreply.github.com>
2023-06-08 17:35:22 +0800

    Update interceptors.go

--- interceptors.go
+++ interceptors.go
@@ -7,18 +7,28 @@ import (
 	"google.golang.org/grpc"
 )
 
+const (
+	AuthMethod = "/auth.AuthService/Validate"
+)
+
 func LoggingInterceptor(logger *logrus.Logger) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+
 		traceID := uuid.New().String()
 		// Log the request body
-		logger.Infof("%s Method: %s, Request body: %v", traceID, info.FullMethod, req)
+		if info.FullMethod != AuthMethod {
+			logger.Infof("%s Method: %s, Request body: %v", traceID, info.FullMethod, req)
+		}
 
 		// Invoke the gRPC handler to process the request.
 		resp, err := handler(context.WithValue(ctx, "traceID", traceID), req)
 
 		if err != nil {
 			logger.Warnf("%s Error: %s ", traceID, err.Error())
-		} else {
+			if info.FullMethod == AuthMethod {
+				logger.Infof("%s Method: %s, Request body: %v", traceID, info.FullMethod, req)
+			}
+		} else if info.FullMethod != AuthMethod {
 			// Log the response body
 			logger.Infof("%s  Method: %s, Response body: %v", traceID, info.FullMethod, resp)
 		}
